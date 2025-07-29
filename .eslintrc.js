3f11ed6285346c6aab21192cc4ab6e8257c72fc5
Masafumi Yokoyama <myokoym@gmail.com>
2025-07-29 10:58:38 +0900

    プロジェクトの初期セットアップ
    TypeScriptベースのNode.jsプロジェクトとして初期化
    - package.jsonにプロジェクト情報と依存関係を定義
    - TypeScript設定（ES2020ターゲット、strict mode）
    - ESLint/Prettier設定でコード品質を確保
    - Jest設定でTypeScriptテストをサポート
    - .gitignoreで不要なファイルを除外
    
    主な依存関係:
    - Commander.js: CLIフレームワーク
    - simple-git: Git操作
    - chalk/ora: ターミナル装飾
    - inquirer: インタラクティブプロンプト
    - Winston: ロギング

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier'
+  ],
+  plugins: ['@typescript-eslint'],
+  env: {
+    node: true,
+    es2020: true,
+    jest: true
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { 
+      argsIgnorePattern: '^_',
+      varsIgnorePattern: '^_'
+    }],
+    'no-console': ['warn', { allow: ['warn', 'error'] }]
+  },
+  ignorePatterns: ['dist', 'node_modules', '*.js']
+};
